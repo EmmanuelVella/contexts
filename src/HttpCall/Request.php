5d81f6435d23cf6c352c5209343c1e39c2897577
Emmanuel Vella <vella.emmanuel@gmail.com>
2015-07-06 14:41:23 +0200

    Return same request across calls

--- src/HttpCall/Request.php
+++ src/HttpCall/Request.php
@@ -7,6 +7,7 @@ use Behat\Mink\Mink;
 class Request
 {
     private $mink;
+    private $request;
 
     public function __construct(Mink $mink)
     {
@@ -20,11 +21,14 @@ class Request
 
     private function getClient()
     {
-        if ($this->mink->getDefaultSessionName() === 'symfony2') {
-            return new Request\Goutte($this->mink);
+        if (null === $this->request) {
+            if ($this->mink->getDefaultSessionName() === 'symfony2') {
+                $this->request = new Request\Goutte($this->mink);
+            } else {
+                $this->request = new Request\BrowserKit($this->mink);
+            }
         }
-        else {
-            return new Request\BrowserKit($this->mink);
-        }
+
+        return $this->request;
     }
 }
